88282c666cceef44014e82ea4b2f41da735b48c1
sky_eats_horizon <sibrkudor2dn@opayq.com>
2019-05-12 11:10:39 +0200

    add par alg with 2 versions. NOT Tested!!

--- implementations.cu
+++ implementations.cu
@@ -23,6 +23,33 @@ __global__ void naive_aproach_one_thread(double *matrix, double *minval, double 
 
 __global__ void naive_aproach_multi_thread(double *matrix, double *minval, double *maxval, int arrlen, int window_size);
 
+class Min_Max_calc
+{
+private:
+	double min,
+		   max;
+
+public:
+	__device__ Min_Max_calc(double *arr, unsigned int length){
+		assert(arr != NULL);
+		min = max = arr[0];
+		for (int i = 1; i < length; ++i)
+		{
+			min = (arr[i] < min)? arr[i] : min;
+			max = (arr[i] > max)? arr[i] : max;
+		}
+	}
+	__device__ ~Min_Max_calc();
+
+	__device__ double getMin(){
+		return min;
+	}
+
+	__device__ double getMax(){
+		return max;
+	}
+};
+
 double naive_aproach_fabian(cuda_matrix *matrix){
 	cudaDeviceProp prop;
 	checkCudaErrors(cudaGetDeviceProperties(&prop, DEV_ID));
@@ -156,6 +183,38 @@ __global__ void naive_aproach_one_thread(double *matrix, double *minval, double 
 		//print_matrixx(maxval, arrlen);	
 		//print_matrixx(minval, arrlen);
 		
+	}
+}
+
+__global__ void par_alg_inc_win(double *matrix, double *minval, double *maxval, int arrlen, int window_size){
+	int tid = threadIdx.x,
+		bid = blockIdx.x,
+		shift_amount = 0;
+	assert(window_size >= MIN_WIN_SIZE);
+
+	int addr_offs = (tid + shift_amount) + bid*window_size*2;
+	while(addr_offs+window_size < arrlen) {
+		Min_Max_calc m((double *)(matrix + addr_offs));
+		minval[addr_offs] = m.getMin();
+		maxval[addr_offs] = m.getMax();
+
+		addr_offs += gridDim.x;
+	}
+}
+
+__global__ void par_alg_inc_blocks(double *matrix, double *minval, double *maxval, int arrlen, int window_size){
+	int tid = threadIdx.x,
+		bid = blockIdx.x,
+		shift_amount = 0;
+	assert(window_size >= MIN_WIN_SIZE);
+
+	int addr_offs = (tid + shift_amount) + bid*window_size;
+	while(addr_offs+window_size < arrlen) {
+		Min_Max_calc m((double *)(matrix + addr_offs));
+		minval[addr_offs] = m.getMin();
+		maxval[addr_offs] = m.getMax();
+
+		addr_offs += window_size;
 	}
 }
 
